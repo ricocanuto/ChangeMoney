02fc473f856239e308a07f5ccf1a1e6ea70c6500
ricocanuto <ricardomoraescanuto@gmail.com>
2023-09-04 15:45:21 -0300

    added-new-currencies

--- conversor.js
+++ conversor.js
@@ -7,30 +7,66 @@ function convertValues() {
     const currencyValueConverted = document.querySelector(".currency-value")
 
     console.log(currencySelect.value)
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    const dolarToday = 4.95
+    const euroToday = 5.34
+    const libraToday = 6.23
+    const ieneToday = 0.04
 
-    if (currencySelect.value == "dolar"){
+    if (currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
         }).format(inputCurrencyValue / dolarToday)
     }
-    if(currencySelect.value == "euro"){
+    if (currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
+    }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+    if (currencySelect.value == "iene") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
+            style: "currency",
+            currency: "JPY"
+        }).format(inputCurrencyValue / ieneToday)
     }
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-   
 
 }
 
+function changeCurrency() {
+    const currencyName = document.querySelector(".currency")
+    const currencyImage = document.querySelector(".currency-img")
 
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar"
+        currencyImage.src="./Assets/estados-unidos (1) 1.png"
+    }
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src="./Assets/logo-euro.png"
+    }
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src="./Assets/logo-moeda-UK.png"
+    }
+    if (currencySelect.value == "iene") {
+        currencyName.innerHTML = "Iene";
+        currencyImage.src="./Assets/logo-moeda-JPN.png";
+    }
+    
+    convertValues()
+}
 
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
